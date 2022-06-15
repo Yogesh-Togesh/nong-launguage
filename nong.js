495dee179795bf5838e9935952e726c90ded502c
Yogesh-Togesh <106044711+Yogesh-Togesh@users.noreply.github.com>
2022-06-15 15:02:23 -0500

    Create nong.js

---
+++ nong.js
@@ -0,0 +1,10 @@
+function
+function mainLine1(mong, correction) {
+correction.mong("*mong");
+}
+function startix() {
+
+}
+function DevCode(write) {
+write.mongCode("-OWNER82sixTogeshOwnerClap");
+}
