0f55ac501313fc3926250505eed102f8c48ff10f
Yogesh-Togesh <106044711+Yogesh-Togesh@users.noreply.github.com>
2022-06-16 11:15:57 -0500

    Update nong.js

--- nong.js
+++ nong.js
@@ -1,12 +1,24 @@
 function main() {
-function mainLine1(nong, correction, name= "nong") {
+function DevCode(write) {
+write.nongCode("*OWNER82sixTogeshOwnerClap");
+}
+  function mainLine1(nong, correction, name= "nong") {
 correction.mong("*" + name);
 }
+  
+  function mainLine2(thingDevs = DevCode, error) {
+  if (devcode = true) {
+  while (devcode = true) {
+  let error = false
+  }
+  }
+  }
+  
 function startix() {
 
 }
-function DevCode(write) {
-write.nongCode("-OWNER82sixTogeshOwnerClap");
-}
 var name = "nong";
+  
+  mainLine1()
+  mainLine2(DevCode())
 }.run(main)
